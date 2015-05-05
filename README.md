1f6090351efdbc88b4c42ff489f2fc6e19d935b8
Luke Scott <luke@webconnex.com>
2015-05-05 15:42:37 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# babel-plugin-forOf-array-only
+Babel plugin that forces forOf transform to be array only.
---
+++ index.js
@@ -0,0 +1,9 @@
+var Transformer = require("babel-core").Transformer;
+var tpath = "babel-core/lib/babel/transformation/transformers";
+var _ForOfStatementArray = require(tpath+"/es6/for-of")._ForOfStatementArray;
+
+module.exports = new Transformer("forOf-array-only", {
+	ForOfStatement: function(node, parent, scope, file) {
+		return _ForOfStatementArray.call(this, node, scope, file);
+	}
+});
